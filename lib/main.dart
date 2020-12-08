a8cb214b9bb8aa5e81577d9a94561531fe0b5d5e
Shilpi kumari <kumarishilpi.nuchange@gmail.com>
2020-12-08 14:04:14 +0530

    uploaded multiple

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class SingleImageUpload extends StatefulWidget {
   @override
   _SingleImageUploadState createState() {
     return _SingleImageUploadState();
+
   }
 }
 
@@ -16,10 +17,14 @@ class _SingleImageUploadState extends State<SingleImageUpload> {
   var endPoint =
       'http://shifajeddah.nuacare.ai/nuacare-core/rest/nuacare/v1/file/resumableUpload/hr?';
   List<Object> images = List<Object>();
+  List<int> allIndexSelectedList=List<int>();
   Future<File> _imageFile;
+  int tempIndex;
+  ImageUploadModel uploadModelTemp;
+
   @override
   void initState() {
-    // TODO: implement initState
+// TODO: implement initState
     super.initState();
     setState(() {
       images.add("Add Image");
@@ -34,20 +39,66 @@ class _SingleImageUploadState extends State<SingleImageUpload> {
       home: new Scaffold(
         appBar: new AppBar(
           centerTitle: true,
-          title: const Text('Upload image'),
+          title: const Text('Click and Upload image'),
         ),
         body: Column(
           children: <Widget>[
+
             Expanded(
               child: buildGridView(),
             ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                allIndexSelectedList.length>=1?Positioned(
+                  right: 20,
+                  top: 50,
+                  child: InkWell(
+                    child: Icon(
+                      Icons.delete,
+                      size: 25,
+                      color: Colors.red,
+                    ),
+                    onTap: () {
+                      setState(() {
+                // images.removeAt( images);
+                      });
+                    },
+                  ),
+                ):Container(),
+                allIndexSelectedList.length>=1?Positioned(
+                  right: 50,
+                  top: 5,
+                  child: InkWell(
+                    child: Icon(
+                      Icons.file_upload,
+                      size: 25,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      // if(allIndexSelectedList.length==1){
+                      //   _upload(uploadModelTemp, tempIndex);
+                      // }else{
+                        for(int i=0; i<=allIndexSelectedList.length;++i){
+                          _uploadMultiple(images, i);
+
+                      }
+
+
+                    },
+                  ),
+                ):Container(),
+              ],
+            )
           ],
         ),
       ),
     );
   }
-
-  void _upload(ImageUploadModel file, index) async {
+  void _uploadMultiple(List<Object> fileList, index) async {
+    ImageUploadModel file = fileList[index];
     String fileName = file.imageFile.path.split('/').last;
     var fileSize = await file.imageFile.length();
     endPoint +=
@@ -94,16 +145,73 @@ class _SingleImageUploadState extends State<SingleImageUpload> {
     });
     Dio dio = new Dio();
     dio.options.headers[HttpHeaders.COOKIE] =
-    "JSONToken=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbiIsImF1ZGllbmNlIjoidW5rbm93biIsImNyZWF0ZWQiOjE2MDY4MTE4MjU0MzgsImV4cCI6MTYwNjg1NTAyNSwiZmFjaWxpdHkiOjB9.0FNqqyhMvZ3Xq6n459q3lA_udOFt665nnhXR33zBMO7xBhzlvDxR3WVp4ifilN2IzxN7wps0p0hMr7yfUy9d8g";
+    "JSONToken=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbiIsImF1ZGllbmNlIjoidW5rbm93biIsImNyZWF0ZWQiOjE2MDc0MDg4ODEzODIsImV4cCI6MTYwNzQ1MjA4MSwiZmFjaWxpdHkiOjB9.7gu7NN8FWDCdJ-6SrbSNSXJz3p0LbUceDsuqVjqFNOzSkdQQn8a-WsegLYsXDEHhdsC6jg5rSH-Kb5nEX3NMAA";
     dio.post(endPoint, data: data).then((response) {
-      print(response.data);
+      print("shilpi $response.$data");
       setState(() {
         ImageUploadModel uploadModel = images[index];
         uploadModel.isUploaded = true;
       });
-      //_getDocument(response.data);
+//_getDocument(response.data);
     }).catchError((error) => {print(error)});
   }
+//   void _upload(ImageUploadModel file, index) async {
+//     String fileName = file.imageFile.path.split('/').last;
+//     var fileSize = await file.imageFile.length();
+//     endPoint +=
+//         "resumableChunkNumber=1&resumableChunkSize=1048576&resumableCurrentChunkSize=" +
+//             fileSize.toString() +
+//             "&resumableTotalSize=" +
+//             fileSize.toString() +
+//             "&resumableType=image%2Fjpeg&resumableIdentifier=" +
+//             fileName +
+//             "&resumableFilename=" +
+//             fileName +
+//             "&resumableRelativePath=" +
+//             fileName +
+//             "&resumableTotalChunks=1";
+//     FormData data = FormData.fromMap({
+//       "resumableChunkNumber": 1,
+//       "resumableChunkSize": 1048576,
+//       "resumableCurrentChunkSize": fileSize,
+//       "resumableTotalSize": fileSize,
+//       "resumableType": "image/jpeg",
+//       "resumableIdentifier": fileName,
+//       "resumableFilename": fileName,
+//       "resumableRelativePath": fileName,
+//       "resumableTotalChunks": 1,
+//       "file": await MultipartFile.fromFile(
+//         file.imageFile.path,
+//         filename: fileName,
+//       ),
+//     });
+//     print({
+//       "resumableChunkNumber": 1,
+//       "resumableChunkSize": 1048576,
+//       "resumableCurrentChunkSize": fileSize,
+//       "resumableTotalSize": fileSize,
+//       "resumableType": "image/jpeg",
+//       "resumableIdentifier": fileName,
+//       "resumableFilename": fileName,
+//       "resumableRelativePath": fileName,
+//       "resumableTotalChunks": 1,
+//       "file": await MultipartFile.fromFile(
+//         file.imageFile.path,
+//         filename: fileName,
+//       ),
+//     });
+//     Dio dio = new Dio();
+// dio.options.headers[HttpHeaders.COOKIE] =
+// "JSONToken=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbiIsImF1ZGllbmNlIjoidW5rbm93biIsImNyZWF0ZWQiOjE2MDczMjQwODk5NzAsImV4cCI6MTYwNzM2NzI4OSwiZmFjaWxpdHkiOjB9.K3JT0XXt3dc_m_Ea7y8q7LzJQaaH-p-KKHz0Y4XXqNno8xAtXiBCnR9TZ_PdxHR3DED_HIH_27cc6ePkG1qgOw";
+//     dio.post(endPoint, data: data).then((response) {
+//       print("shilpi $response.$data");
+//       setState(() {
+//         ImageUploadModel uploadModel = images[index];
+//         uploadModel.isUploaded = true;
+//       });
+// //_getDocument(response.data);
+//     }).catchError((error) => {print(error)});
+//   }
 
   Widget buildGridView() {
     return GridView.count(
@@ -111,6 +219,7 @@ class _SingleImageUploadState extends State<SingleImageUpload> {
       crossAxisCount: 3,
       childAspectRatio: 1,
       children: List.generate(images.length, (index) {
+
         if (images[index] is ImageUploadModel) {
           ImageUploadModel uploadModel = images[index];
           if (uploadModel.isUploaded == false) {
@@ -118,41 +227,45 @@ class _SingleImageUploadState extends State<SingleImageUpload> {
               clipBehavior: Clip.antiAlias,
               child: Stack(
                 children: <Widget>[
-                  Image.file(
-                    uploadModel.imageFile,
-                    width: 300,
-                    height: 300,
+                  GestureDetector(
+                    onDoubleTap: (){
+                      setState(() {
+                        print("image selected of index "+index.toString());
+                        allIndexSelectedList.add(index);
+                        uploadModel.isSelected=true;
+                        tempIndex=index;
+                        uploadModelTemp=uploadModel;
+                      });
+
+
+                    },
+                    child: Image.file(
+                      uploadModel.imageFile,
+                      width: 300,
+                      height: 300,
+                    ),
                   ),
+            //     GestureDetector(
+            //
+            //     // When the child is tapped, show a snackbar.
+            //         onTap: () {
+            //           // here, Scaffold.of(context) returns the locally created Scaffold
+            //           Scaffold.of(context).showSnackBar(SnackBar(
+            //               content: Text('Hello.')
+            //           ));
+            //         }
+            // )
+                  uploadModel.isSelected?
                   Positioned(
                     right: 5,
                     top: 5,
-                    child: InkWell(
-                      child: Icon(
-                        Icons.remove_circle,
-                        size: 20,
-                        color: Colors.red,
-                      ),
-                      onTap: () {
-                        setState(() {
-                          images.replaceRange(index, index + 1, ['Add Image']);
-                        });
-                      },
+                    child: Icon(
+                      Icons.access_time_rounded,
+                      size: 25,
+                      color: Colors.green,
                     ),
-                  ),
-                  Positioned(
-                    right: 30,
-                    top: 5,
-                    child: InkWell(
-                      child: Icon(
-                        Icons.file_upload,
-                        size: 20,
-                        color: Colors.blue,
-                      ),
-                      onTap: () {
-                        _upload(uploadModel, index);
-                      },
-                    ),
-                  ),
+                  ):Container(),
+
                 ],
               ),
             );
@@ -201,12 +314,21 @@ class _SingleImageUploadState extends State<SingleImageUpload> {
   Future _onAddImageClick(int index) async {
     setState(() {
       _imageFile = ImagePicker.pickImage(source: ImageSource.camera, maxWidth: 480, maxHeight: 600);
-      getFileImage(index);
+// getFileImage(index);
+      _imageFile.then((file) {
+        if (file != null) {
+          setState(() {
+            print(" shilpi filename$_imageFile");
+            getFileImage(index);
+          });
+        }
+      });
+
     });
   }
 
   void getFileImage(int index) async {
-//    var dir = await path_provider.getTemporaryDirectory();
+// var dir = await path_provider.getTemporaryDirectory();
 
     _imageFile.then((file) async {
       images.add("Add Image");
@@ -216,6 +338,7 @@ class _SingleImageUploadState extends State<SingleImageUpload> {
         imageUpload.uploading = false;
         imageUpload.imageFile = file;
         imageUpload.imageUrl = '';
+        imageUpload.isSelected = false;
         images.replaceRange(index, index + 1, [imageUpload]);
       });
     });
@@ -227,11 +350,13 @@ class ImageUploadModel {
   bool uploading;
   File imageFile;
   String imageUrl;
+  bool isSelected;
 
   ImageUploadModel({
     this.isUploaded,
     this.uploading,
     this.imageFile,
     this.imageUrl,
+    this.isSelected,
   });
 }
